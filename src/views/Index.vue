e594354121993d1e83273590ec90abb920bbe9c8
XiangZhifa <974688383@qq.com>
2021-01-06 08:49:59 +0800

    Vue@Next setUp和ref的基本使用

---
+++ src/views/Index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="about">
+    <p>Vue@Next</p>
+    <p>setUp-{{num}}</p>
+    <div>
+      <p>count-{{count}}</p>
+      <button @click="updateCount">count++</button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+  import {defineComponent, ref} from 'vue';
+
+  export default defineComponent({
+    name: 'Index',
+    setup() {
+      const num = 10;
+      //ref函数，定义一个响应式数据，返回reference对象，对象中包含value属性，通过操作value属性，实现双向绑定
+      //ref一般用来定义基本数据类型的响应式数据
+      let count = ref(0);
+
+      function updateCount() {
+        console.log(count);
+        count.value++
+      }
+      return {
+        num,
+        count,
+        updateCount
+      }
+    }
+  })
+</script>
